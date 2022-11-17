33d454639212ea45251844dcfd8957d39577fdf8
mahiuddin22 <mahiuddin.noyon@gmail.com>
2022-11-17 14:22:16 +0600

    complete mastering admin template

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,6 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AdminController;
 
 
-Route::get('admin',[AdminController::class,'index'])->name('admin.login');
+Route::get('/admin',[AdminController::class,'index'])->name('admin.login');
+Route::get('/dashboard',[AdminController::class,'dashboard'])->name('admin.dashboard');
 Route::get('/',[HomeController::class,'index'])->name('home');
