33b290175628ab1a8f9232a4679afedb26cf3471
mahiuddin22 <mahiuddin.noyon@gmail.com>
2022-11-17 13:43:18 +0600

    page mastering admin template

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
 
-
-
+Route::get('admin',[AdminController::class,'index'])->name('admin.login');
 Route::get('/',[HomeController::class,'index'])->name('home');
