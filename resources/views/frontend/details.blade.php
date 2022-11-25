2b86a270c951a19545d77b75561f02928c81fd6c
mahiuddin22 <mahiuddin.noyon@gmail.com>
2022-11-26 00:53:26 +0600

    fixed product color and size

--- resources/views/frontend/details.blade.php
+++ resources/views/frontend/details.blade.php
@@ -87,13 +87,17 @@
                             <label>
                                 Size
                                 <select class="input-select">
-                                    <option value="0">{{$product->size}}</option>
+                                    @foreach(json_decode($sizes->size) as $value))
+                                    <option value="0">{{$value}}</option>
+                                    @endforeach
                                 </select>
                             </label>
                             <label>
                                 Color
                                 <select class="input-select">
-                                    <option value="0">{{$product->color}}</option>
+                                    @foreach(json_decode($colors->color) as $value))
+                                    <option value="0">{{$value}}</option>
+                                    @endforeach
                                 </select>
                             </label>
                         </div>
@@ -379,37 +383,37 @@
                 </div>
 
                 <!-- product -->
-                    <div class="col-md-3 col-xs-6">
-                        <div class="product">
-                            <div class="product-img">
-                                <img src="{{asset('./img/product01.png')}}" alt="">
-                                <div class="product-label">
-                                    <span class="sale">-30%</span>
-                                </div>
-                            </div>
-                            <div class="product-body">
-                                <p class="product-category">Category</p>
-                                <h3 class="product-name"><a href="#">product name goes here</a></h3>
-                                <h4 class="product-price">$980.00
-                                    <del class="product-old-price">$990.00</del>
-                                </h4>
-                                <div class="product-rating">
-                                </div>
-                                <div class="product-btns">
-                                    <button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span>
-                                    </button>
-                                    <button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">add to compare</span>
-                                    </button>
-                                    <button class="quick-view"><i class="fa fa-eye"></i><span
-                                            class="tooltipp">quick view</span></button>
-                                </div>
-                            </div>
-                            <div class="add-to-cart">
-                                <button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+                <div class="col-md-3 col-xs-6">
+                    <div class="product">
+                        <div class="product-img">
+                            <img src="{{asset('./img/product01.png')}}" alt="">
+                            <div class="product-label">
+                                <span class="sale">-30%</span>
                             </div>
                         </div>
+                        <div class="product-body">
+                            <p class="product-category">Category</p>
+                            <h3 class="product-name"><a href="#">product name goes here</a></h3>
+                            <h4 class="product-price">$980.00
+                                <del class="product-old-price">$990.00</del>
+                            </h4>
+                            <div class="product-rating">
+                            </div>
+                            <div class="product-btns">
+                                <button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span>
+                                </button>
+                                <button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">add to compare</span>
+                                </button>
+                                <button class="quick-view"><i class="fa fa-eye"></i><span
+                                        class="tooltipp">quick view</span></button>
+                            </div>
+                        </div>
+                        <div class="add-to-cart">
+                            <button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+                        </div>
                     </div>
-                    <!-- /product -->
+                </div>
+                <!-- /product -->
             </div>
             <!-- /row -->
         </div>
